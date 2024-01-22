10cd7d690d6a768c1a12c6afccb07216ce30b877
roland444 <123905024+roland444@users.noreply.github.com>
2024-01-22 19:51:19 +0100

    Added jquery library

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const _stopwatch = document.querySelector(".stopwatch");
 const _startBtn = document.querySelector(".start");
 const _resetBtn = document.querySelector(".reset");
+
+let timer = new Timer();
+
+    timer.start();
+
+    timer.addEventListener('secondsUpdated', function(e) {
+        $('#basicUsage').html(timer.getTimeValues().toString());
+    });
